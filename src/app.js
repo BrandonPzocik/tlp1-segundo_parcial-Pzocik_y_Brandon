ed030be0bd99b919f46f37be9a4e7df56145cd93
Brandon <pzocikbrandon@gmail.com>
2024-06-27 15:40:05 -0300

    se crearon las rutas get, post, put y delete

--- src/app.js
+++ src/app.js
@@ -5,6 +5,41 @@ const app = express();
 
 app.use(express.json())
 
+//obtner todos los libros
+app.get("/books", (req, res) => {
+    res.json(bd)
+})
+//obtener el book por su id 
+app.get("/books/:id", (req, res)=> {
+    const id = parseInt(req.params.id)
+    const obtenerBook = bd.find( (book) => book.id === id) 
+    res.json(obtenerBook)
+})
+//crear un nuevo book
+app.post("/books", (req, res) => {
+    const {id, titulo, autor, año} = req.body 
+    const nuevoBook = bd.push( {id:id, titulo:titulo, autor:autor, año:año})
+    res.json({mensaje:"book creado con exito"})
+})
+app.put("/books/:id", (req, res) => {
+    const id = parseInt(req.params.id)
+    const {titulo, autor, año} = req.body
+    const actualizar = bd.find( (book) => book.id === id)
+    actualizar.titulo = titulo 
+    actualizar.autor = autor 
+    actualizar.año = año 
+    res.json({mensaje:"book actualizado con exito"})
+    
+})
+//eliminar book
+app.delete("/books/:id", (req, res) => {
+    const id = parseInt(req.params.id)
+    const obtenerBook = bd.find( (book) => book.id === id)
+    const bookIndex = bd.indexOf(obtenerBook)
+    const eliminarBook = bd.splice(bookIndex, 1)
+    res.json({mensaje:"book eliminado con exito"})
+})
+
 app.listen(4000, () => {
     console.log("Servidor corriendo")
 })
---
+++ src/bd.js
@@ -0,0 +1,23 @@
+const bd = [
+    {
+    id: 1, 
+    titulo: "Don Quijote de la Mancha ",
+    autor: "Miguel de Cervantes",
+    año: 2015
+
+    },
+    {
+    id: 2, 
+    titulo: "50 sombras de grey",
+    autor: "Erika James",
+    año: 2015
+
+    },
+    {
+    id: 3, 
+    titulo: "Diario de Ana Frank",
+    autor: "Ana frank",
+    año: 2005
+    }
+]
+module.exports = bd; 
